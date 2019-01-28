e49206a08dd73ca467839896e15b84b82d2374a7
reneeb <info@perl-services.de>
2019-01-28 07:22:17 +0100

    prepare release

--- lib/EPublisher.pm
+++ lib/EPublisher.pm
@@ -10,7 +10,7 @@ use EPublisher::Config;
 use EPublisher::Source;
 use EPublisher::Target;
 
-our $VERSION = 1.26;
+our $VERSION = 1.27;
 
 sub new{
     my ($class,%args) = @_;
--- lib/EPublisher/Target.pm
+++ lib/EPublisher/Target.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use Carp;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub new{
     my ($class,$args) = @_;
